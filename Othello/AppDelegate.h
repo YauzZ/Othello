26070016851fe7cff618865bc9567bfb8a5586db
Colin Yang Hong <colinyang@nmg.com.hk>
2013-05-22 16:19:09 +0800

    Initial Commit

---
+++ Othello/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Othello
+//
+//  Created by Colin Yang Hong on 13-5-22.
+//  Copyright (c) 2013年 Colin Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Othello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Othello
+//
+//  Created by Colin Yang Hong on 13-5-22.
+//  Copyright (c) 2013年 Colin Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
